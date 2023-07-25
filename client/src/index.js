179b5c75817dcbd69ef467b894ef4d62e2324774
Daquanj <Daquanj.dev@gmail.com>
2023-07-24 23:27:07 -0400

    updated server .ignore, initialized client redux store

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import reducers from './redux/reducers/index';
+import { legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// GLOBAL REDUX STORE - window initializes redux dev tools logs
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+
+// USING PROVIDER CONNECTING STORE TO APP
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ client/src/redux/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+
+// COMBINE ALL APP REDUCERS 
+const reducers = combineReducers({});
+
+export default reducers;
