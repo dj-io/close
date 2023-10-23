c98d076f6dba35bc64a599fda92f8846380f32a2
Daquanj <Daquanj.dev@gmail.com>
2023-10-23 19:11:23 -0400

    bug-fixes: post card ui

--- client/src/common/components/cards/PostCard.Styles.ts
+++ client/src/common/components/cards/PostCard.Styles.ts
@@ -49,7 +49,7 @@ export const CardWrapper = styled(Card)((props: any) => ({
         boxShadow: 'none',
 
         '@media (max-width: 790px)': {
-            maxWidth: !props.expanded ? 335 : 325,
+            maxWidth: !props.expanded ? 335 : 305,
             marginTop: '0'
         }
     }
