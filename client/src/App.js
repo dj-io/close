d4f4f1ee6ce7a0faabff280a8f7d82962f67b5df
Daquanj <Daquanj.dev@gmail.com>
2023-10-22 23:07:35 -0400

    troubleshooting: react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Routes, Route } from 'react-router-dom';
 import { Post } from './components/profile/Post.tsx';
 import { Signup, Signin, Nav, Profile, Home, Find, Share, SignOut } from './components/index.ts';
 import Friends from './components/find/Friends.tsx';
@@ -21,17 +21,19 @@ const App = ({
         {loggedIn && <Nav user={user} />}
         {loggedIn && <Find />}
         {loggedIn && <SignOut />}
-        <Routes>
-          <Route exact path='/' element={hasAccount ? <Signin /> : <Signup />} />
-          <Route element={<Protected hasAccount={hasAccount} />}>
-            <Route exact path='/home' element={<Home />} />
-            <Route key='home-post' path='/home/:post' element={<Post currentUser={user} />} />
-            <Route path='/share' element={<Share />} />
-            <Route path='/profile/:profileId' element={<Profile />} />
-            <Route key='profile-post' exact path='/user/:post' element={<Post currentUser={user} />} />
-            <Route exact path='/:name' element={<Friends currentUser={user} profiles={profiles} />} />
-          </Route>
-        </Routes>
+        <Switch>
+          <Routes>
+            <Route exact path='/' element={hasAccount ? <Signin /> : <Signup />} />
+            <Route element={<Protected hasAccount={hasAccount} />}>
+              <Route exact path='/home' element={<Home />} />
+              <Route key='home-post' path='/home/:post' element={<Post currentUser={user} />} />
+              <Route path='/share' element={<Share />} />
+              <Route path='/profile/:profileId' element={<Profile />} />
+              <Route key='profile-post' exact path='/user/:post' element={<Post currentUser={user} />} />
+              <Route exact path='/:name' element={<Friends currentUser={user} profiles={profiles} />} />
+            </Route>
+          </Routes>
+        </Switch>
       </Router>
     </div>
   );
